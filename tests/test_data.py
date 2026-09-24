5668267a2e4c889840499526ce5a107938c4b927
toni646 <tlb.toni@gmail.com>
2026-09-24 17:18:21 +0200

    Translate test comment to English

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,5 +2,5 @@ import pytest  # noqa: F401
 
 
 def test_placeholder():
-    # TODO: afegir tests de les metriques (issue #7)
+    # TODO: add tests for the evaluation metrics (issue #7)
     assert True
